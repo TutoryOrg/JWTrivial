5c8a87759b94fe5df8351383fd38fbc6b9a3bfff
STAYN ALEJANDRO ALCOCER VALENCIA <stalyn.alcocer@idrica.com>
2021-12-11 18:33:14 +0100

    :sparkles:

---
+++ jwgame/src/constants/index.ts
@@ -0,0 +1,49 @@
+export const fontSizes = {
+  xSmall: 12,
+  small: 14,
+  normal: 16,
+  large: 18,
+  xLarge: 20,
+  xxLarge: 24,
+  xxxLarge: 28,
+  xxxxLarge: 32,
+};
+
+export const grid = 8;
+export const grid2x = 16;
+export const grid3x = 24;
+export const grid4x = 32;
+
+export const fontFamilies = {
+  Nunito: "Nunito-Regular",
+  NunitoBold: "Nunito-Bold",
+}
+
+export const colors = {
+  black: "#000",
+  white: "#FFF",
+  darkGrey: "#999999",
+  yellow: "#FFCC00",
+  primaryGray: "#555555",
+  primaryBlack: "#20201E",
+  backgroundGray: "#F5F5F3",
+  lightGrey: "#F4F4F4",
+  lightYellow: "#FFF1D0",
+  platinum: "#DEE2E6",
+  green: "#13E399",
+  fadedGreen: "#c7f8e7",
+  fadedGrey: "#cccccc",
+  darkRed: "#BF351F",
+  lightRed: "#FBCAC1",
+  red: "#F04E30",
+  toastRed: "#FF3C19",
+  yellowConfirm: "#FFF1CC",
+  lightBlue: "#DCEAF8",
+  lightOrange: "#FFA700",
+  paleYellow: "#FFEB9A",
+  orange: "#FF6D00",
+  lightPink: "#FFC8BF",
+  paleOrange: "#FFCFAA",
+  failure: "#E32704",
+  paleBlue: "#DEEAF7",
+};
--- jwgame/src/screens/HomeScreen/index.tsx
+++ jwgame/src/screens/HomeScreen/index.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import {Text} from '../../components/Text';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {CompositeScreenProps} from '@react-navigation/native';
 import {GameStackParamList} from '@navigation/GameNavigator';
 import {MainStackParamList} from '@navigation/MainNavigator';
 import {useTranslation} from 'react-i18next';
+import styled from 'styled-components/native';
 
 const HomeView = styled.View`
   flex: 1;
-  background-color: ${props => props.theme.viewBg};
   align-items: center;
+  background-color: ${props => props.theme.viewBg};
 `;
 
-const HeaderText = styled.Text`
-  font-size: 28px;
+const HeaderText = styled(Text)`
+  font-size: 34px;
+  font-family: "Nunito-Regular";
   color: ${props => props.theme.textColor};
 `;
 
 const SubHeaderText = styled.Text`
-  font-size: 18px;
+  font-size: 24px;
+  font-family: "Nunito-Regular";
   color: ${props => props.theme.textColor};
 `;
 
