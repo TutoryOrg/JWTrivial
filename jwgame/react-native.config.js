cc562d134f6476dd26c977aead8f209b4880d929
STAYN ALEJANDRO ALCOCER VALENCIA <stalyn.alcocer@idrica.com>
2021-12-11 20:11:40 +0100

    update

--- jwgame/react-native.config.js
+++ jwgame/react-native.config.js
@@ -4,4 +4,5 @@ module.exports = {
     android: {}, // grouped into "project"
   },
   assets: ['./src/assets/fonts'],
+  dependencies: {}, // make sure this dependencies are all valid installed packages or empty if you don't need it
 };
