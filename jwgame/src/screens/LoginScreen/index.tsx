258e3c64f9b50bfedbcf50cd675371bf3e0e2d12
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-07-04 10:34:32 +0200

    prettify && lint

--- jwgame/src/screens/LoginScreen/index.tsx
+++ jwgame/src/screens/LoginScreen/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Platform} from 'react-native';
 import {useTranslation} from 'react-i18next';
 import {HeaderText} from './LoginScreen.UI';
 import {BgView, TxtInputUser, TxtInputPassw, LoginButton} from './LoginScreen.UI';
@@ -8,7 +7,7 @@ function LoginScreen(): JSX.Element {
     const {t} = useTranslation();
 
     return (
-        <BgView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <BgView>
             <HeaderText>{t('login_admin')}</HeaderText>
             <TxtInputUser label={t('user_name')} placeHolder={t('user_name')} />
             <TxtInputPassw isPassw label={t('password')} placeHolder={t('password')} />
