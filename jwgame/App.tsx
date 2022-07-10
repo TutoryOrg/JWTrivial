86a9e2eb010a6a80fb465a75d2e7b503a7462487
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-07-10 12:39:38 +0200

    Added Storybook on React Native :fire: :rocket:

--- jwgame/App.tsx
+++ jwgame/App.tsx
@@ -1,24 +1,26 @@
-/**
- * @author stalynAlejandro
- */
+// /**
+//  * @author stalynAlejandro
+//  */
 
-import 'react-native-gesture-handler';
-import React from 'react';
-import {ThemeProvider} from 'styled-components';
-import {useColorScheme} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {MainNavigator} from './src/navigation/MainNavigator';
-import {lightTheme, darkTheme} from './src/themes';
+// import 'react-native-gesture-handler';
+// import React from 'react';
+// import {ThemeProvider} from 'styled-components';
+// import {useColorScheme} from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native';
+// import {MainNavigator} from './src/navigation/MainNavigator';
+// import {lightTheme, darkTheme} from './src/themes';
 
-const App = (): JSX.Element => {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
-            <NavigationContainer>
-                <MainNavigator />
-            </NavigationContainer>
-        </ThemeProvider>
-    );
-};
+// const App = (): JSX.Element => {
+//     const isDarkMode = useColorScheme() === 'dark';
+//     return (
+//         <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+//             <NavigationContainer>
+//                 <MainNavigator />
+//             </NavigationContainer>
+//         </ThemeProvider>
+//     );
+// };
 
-export default App;
+// export default App;
+import StorybookUIRoot from './.ondevice/Storybook';
+export {StorybookUIRoot as default};
