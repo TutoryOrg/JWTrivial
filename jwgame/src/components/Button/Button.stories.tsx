84274a6aacc19b1b317e2ebb095e544932e04d55
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-07-10 10:02:04 +0200

    Button Stories

---
+++ jwgame/src/components/Button/Button.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {lightTheme, darkTheme} from '../../themes';
+import {ThemeProvider} from 'styled-components';
+import {Button} from './index';
+
+export default {
+    title: 'components/Button',
+    component: Button,
+} as ComponentMeta<typeof Button>;
+
+export const Basic_darkTheme: ComponentStory<typeof Button> = args => {
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <Button {...args} />
+        </ThemeProvider>
+    );
+};
+
+export const Basic_lightTheme: ComponentStory<typeof Button> = args => {
+    return (
+        <ThemeProvider theme={lightTheme}>
+            <Button {...args} />
+        </ThemeProvider>
+    );
+};
+
+Basic_darkTheme.args = {
+    text: 'Hello World',
+};
+
+Basic_lightTheme.args = {
+    text: 'Hello World',
+};
