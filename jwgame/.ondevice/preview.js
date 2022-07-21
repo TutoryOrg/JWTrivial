5b47cb678c1808259387599bff21631548ce7cd2
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-07-21 15:11:30 +0200

    change default background on storybbook

--- jwgame/.ondevice/preview.js
+++ jwgame/.ondevice/preview.js
@@ -1,9 +1,10 @@
 import {withBackgrounds} from '@storybook/addon-ondevice-backgrounds';
+
 export const decorators = [withBackgrounds];
 export const parameters = {
     backgrounds: [
-        {name: 'default', value: '#A3ABFF', default: true},
-        {name: 'white', value: '#FFFFFF'},
+        {name: 'default', value: '#FFFFFF', default: true},
+        {name: 'purple', value: '#A3ABFF'},
         {name: 'warm', value: 'hotpink'},
         {name: 'cool', value: 'deepskyblue'},
     ],
