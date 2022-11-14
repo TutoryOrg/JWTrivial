f48117db28a05a8eb3252630327e2683441e1651
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-11-13 19:36:34 -0500

    :hammer:

--- jwgame/src/hooks/useQuestions.ts
+++ jwgame/src/hooks/useQuestions.ts
@@ -18,6 +18,7 @@ export function useQuestions({typeQuestion}: IuseQuestions): QuestionEntry[] {
                 });
             setQuestions(res);
         }
+
         fetchQuestions();
     }, []);
 
