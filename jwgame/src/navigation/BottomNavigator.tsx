350d838ae21983b4eca33ecb29190c40d2bdd2da
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-12-12 00:08:03 +0100

    add icons to bottombar

--- jwgame/src/navigation/BottomNavigator.tsx
+++ jwgame/src/navigation/BottomNavigator.tsx
@@ -1,29 +1,66 @@
 import React from 'react';
 import {Screens} from './constants';
 import {MenuStack} from './MenuStack';
+import {useTranslation} from 'react-i18next';
 import {SettingsScreen} from 'screens/SettingsScreen';
 import {ProfileScreen} from 'screens/ProfileScreen';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {scale} from 'utils/scaleFunctions';
+import {gridSizes} from 'utils/constants';
+import styled from 'styled-components/native';
 
 const Tab = createBottomTabNavigator();
 
+export const BottomIcon = styled.Image`
+    width: ${scale(gridSizes.grid4x)}px;
+    height: ${scale(gridSizes.grid4x)}px;
+`;
+
 export function BottomNavigator(): JSX.Element {
+    const {t} = useTranslation();
     return (
         <Tab.Navigator>
             <Tab.Screen
                 name={Screens.MenuStack}
                 component={MenuStack}
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: t('menu'),
+                    tabBarIcon: ({focused}) =>
+                        focused ? (
+                            <BottomIcon source={require('assets/icons/home_active.png')} />
+                        ) : (
+                            <BottomIcon source={require('assets/icons/home.png')} />
+                        ),
+                }}
             />
             <Tab.Screen
                 name={Screens.SettingsScreen}
                 component={SettingsScreen}
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: t('settings'),
+                    tabBarIcon: ({focused}) =>
+                        focused ? (
+                            <BottomIcon source={require('assets/icons/settings_active.png')} />
+                        ) : (
+                            <BottomIcon source={require('assets/icons/settings.png')} />
+                        ),
+                }}
             />
             <Tab.Screen
                 name={Screens.ProfileScreen}
                 component={ProfileScreen}
-                options={{headerShown: false}}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: t('profile'),
+                    tabBarIcon: ({focused}) =>
+                        focused ? (
+                            <BottomIcon source={require('assets/icons/user_active.png')} />
+                        ) : (
+                            <BottomIcon source={require('assets/icons/user.png')} />
+                        ),
+                }}
             />
         </Tab.Navigator>
     );
