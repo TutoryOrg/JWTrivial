9e1ba2b769d684daaea95ae7235935d2fbc6661b
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-12-12 00:36:51 +0100

    :format:

--- jwgame/i18n.js
+++ jwgame/i18n.js
@@ -1,28 +1,28 @@
-import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
-import {NativeModules, Platform} from "react-native";
-import {resources} from "./src/translations/resources";
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import {NativeModules, Platform} from 'react-native';
+import {resources} from './src/translations/resources';
 
 let locale =
-    Platform.OS === "ios"
+    Platform.OS === 'ios'
         ? NativeModules.SettingsManager.settings.AppleLocale
         : NativeModules.I18nManager.localeIdentifier;
 
 if (locale === undefined) {
     locale = NativeModules.SettingsManager.settings.AppleLanguages[0];
     if (locale === undefined) {
-        locale = "en";
+        locale = 'en';
     }
 }
 
 i18n.use(initReactI18next).init({
     resources,
     lng: locale.substring(0, 2),
-    fallbackLng: ["es", "en"],
+    fallbackLng: ['es', 'en'],
     interpolation: {
         escapeValue: false,
     },
-    compatibilityJSON: "v3",
+    compatibilityJSON: 'v3',
 });
 
 export default i18n;
