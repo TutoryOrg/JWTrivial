b8af4d7ccf146b8c73efb8ec43a461d6b9086bf7
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2022-12-17 20:24:38 +0100

    :hammer:

---
+++ jwgame/src/screens/index.ts
@@ -0,0 +1,8 @@
+import {GameScreen} from './GameScreen';
+import {HomeScreen} from './HomeScreen';
+import {LoginScreen} from './LoginScreen';
+import {MenuScreen} from './MenuScreen';
+import {ProfileScreen} from './ProfileScreen';
+import {SettingsScreen} from './SettingsScreen';
+
+export {GameScreen, HomeScreen, LoginScreen, MenuScreen, ProfileScreen, SettingsScreen};
