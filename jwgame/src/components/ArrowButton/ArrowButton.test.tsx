e45e2d239a696abdff1ddae7fbd2a7d08761e8d2
ALEJANDRO ALCOCER <saavalencia97@gmail.com>
2023-02-09 18:38:57 +0100

    :fire:

--- jwgame/src/components/ArrowButton/ArrowButton.test.tsx
+++ jwgame/src/components/ArrowButton/ArrowButton.test.tsx
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import {ArrowButton, directionTypes} from './index';
+import {ArrowButton} from './index';
 
 import renderer from 'react-test-renderer';
 
