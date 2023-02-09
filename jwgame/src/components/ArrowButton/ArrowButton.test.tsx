e39069ae7bb84ce26d3383d6d96dd01dcfeef7ef
ALEJANDRO ALCOCER <saavalencia97@gmail.com>
2023-02-09 18:35:27 +0100

    Add test coverage

--- jwgame/src/components/ArrowButton/ArrowButton.test.tsx
+++ jwgame/src/components/ArrowButton/ArrowButton.test.tsx
@@ -1,10 +1,28 @@
 import 'react-native';
 import React from 'react';
-import {ArrowButton} from './index';
+import {ArrowButton, directionTypes} from './index';
+
 import renderer from 'react-test-renderer';
 
-describe('test ArrowButton', () => {
-    it('test Renders', () => {
-        renderer.create(<ArrowButton onPress={() => console.log('onPress')} />);
+describe('ArrowButton', () => {
+    const defaultProps = {
+        directionArrow: undefined,
+        onPress: () => console.log(''),
+    };
+
+    it('components renders', () => {
+        renderer.create(<ArrowButton {...defaultProps} />);
+    });
+
+    it('components renders directionArrow up', () => {
+        renderer.create(<ArrowButton {...defaultProps} directionArrow="top" />);
+    });
+
+    it('test renders directionArrow down', () => {
+        renderer.create(<ArrowButton {...defaultProps} directionArrow="down" />);
+    });
+
+    it('test renders directionArrow right', () => {
+        renderer.create(<ArrowButton {...defaultProps} directionArrow="right" />);
     });
 });
