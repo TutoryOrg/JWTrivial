f61e4b1cab7f3063bf16715d23873ebbb39a6cb6
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-03-13 13:15:16 +0100

    :hammer: :boom: :fire:

--- jwback/src/app.module.ts
+++ jwback/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DogsController } from './dogs/dogs.controller';
-import { CatsController } from './cats/cats.controller';
 
 @Module({
     imports: [],
-    controllers: [AppController, DogsController, CatsController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
 
--- jwback/src/main.ts
+++ jwback/src/main.ts
@@ -6,19 +6,3 @@ async function bootstrap() {
     await app.listen(3010);
 }
 bootstrap();
-
-/*
- * The entry file of the application which uses the core
- * function NestFactory to create a Nest application instance
- *
- * NestFactory exposes a few static methods that allow creating an application.
- * instance.
- *
- * The create() method returns an application object. This object provides
- * a set of methods which are described in the comming chapters.
- *
- *
- *
- *
- *
- */
