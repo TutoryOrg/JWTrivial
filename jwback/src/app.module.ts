c9987fd1030417df13221413139ac9d75c49f637
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-03-30 20:10:36 +0200

    init back :fire:

--- jwback/src/app.module.ts
+++ jwback/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/jwdb')],
     controllers: [],
     providers: [],
 })
