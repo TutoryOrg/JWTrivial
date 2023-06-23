2302dcdeb2e3ffbb12e19f3a4fe630eb142e11c1
STAYN ALEJANDRO ALCOCER VALENCIA <saavalencia97@gmail.com>
2023-06-23 11:51:05 +0200

    :sparkles:

--- jwgame/.storybook/main.js
+++ jwgame/.storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
-  stories: ["../src/components/**/*.stories.?(ts|tsx|js|jsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-react-native-web",
-  ],
-  framework: "@storybook/react",
+    stories: ['../src/components/**/*.stories.?(ts|tsx|js|jsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-react-native-web',
+    ],
+    framework: '@storybook/react',
 };
